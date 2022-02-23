a2dfcc6b05f6fe7a2432e543fb7716178f58d0ab
Franck Fernandez <franck.fernandez@gmail.com>
2022-02-23 19:52:29 +0100

    Create Counter component

---
+++ components/Counter.tsx
@@ -0,0 +1,23 @@
+import { animate } from 'framer-motion';
+import { memo, useEffect, useRef } from 'react';
+
+export default function Counter({ from, to }: { from: number; to: number }) {
+  const nodeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const node = nodeRef.current;
+
+    if (node) {
+      const controls = animate(from, to, {
+        duration: 1,
+        onUpdate(value) {
+          node.textContent = parseInt(value.toFixed(0)).toLocaleString();
+        },
+      });
+
+      return () => controls.stop();
+    }
+  }, [from, to]);
+
+  return <div ref={nodeRef} />;
+}
