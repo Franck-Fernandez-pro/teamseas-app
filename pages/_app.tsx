2f436e41ad05fd7a88b9cfab7f9761313386afa8
Franck Fernandez <franck.fernandez@gmail.com>
2022-02-23 19:27:32 +0100

    install montserrat font and use it in ChakraUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import '@fontsource/montserrat/700.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/300.css';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Montserrat',
+    body: 'Montserrat',
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
