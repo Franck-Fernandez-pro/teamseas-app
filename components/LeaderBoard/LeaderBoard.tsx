7b8217f02efdf260de83916add96f6b6c14445a7
Franck Fernandez <franck.fernandez@gmail.com>
2022-03-02 11:31:36 +0100

    Fetch Donations

--- components/LeaderBoard/LeaderBoard.tsx
+++ components/LeaderBoard/LeaderBoard.tsx
@@ -1,63 +1,57 @@
-import { memo } from 'react';
+import { Key, memo, useState } from 'react';
 import { Box, Heading, VStack } from '@chakra-ui/react';
 import LeaderBoardItem from './LeaderBoardItem';
+import { gql, useQuery } from '@apollo/client';
+import { Donation, Donations } from '../../types';
+
+const DONATIONS_QUERY = gql`
+  query Donations($orderBy: OrderByParams) {
+    donations(orderBy: $orderBy) {
+      id
+      count
+      displayName
+      team
+      message
+      createdAt
+    }
+  }
+`;
+
+interface QueryVariable {
+  orderBy: {
+    field: string;
+    direction: string;
+  };
+}
 
 interface Props {}
 
 function LeaderBoard({}: Props) {
+  const [field, setField] = useState<string>('createdAt');
+  const { data, error, loading } = useQuery<Donations, QueryVariable>(
+    DONATIONS_QUERY,
+    {
+      variables: {
+        orderBy: {
+          field,
+          direction: 'asc',
+        },
+      },
+    }
+  );
+
+  if (error) return <div>Something went wrong...</div>;
+  if (loading || !data) return <div>Loading...</div>;
+
   return (
     <Box w="100%">
       <Heading>LeaderBoard</Heading>
 
       <VStack spacing={4}>
-        <LeaderBoardItem
-          donation={{
-            id: 12,
-            count: 200,
-            displayName: 'Toto',
-            email: 'toto@gmail.com',
-            mobile: 'mobile',
-            team: 'team',
-            message: 'message',
-            createdAt: "2022-02-24T14:24:08.311Z",
-          }}
-        />
-        <LeaderBoardItem
-          donation={{
-            id: 12,
-            count: 200,
-            displayName: 'Toto',
-            email: 'toto@gmail.com',
-            mobile: 'mobile',
-            team: 'team',
-            message: 'message',
-            createdAt: "2022-02-24T14:24:08.311Z",
-          }}
-        />
-        <LeaderBoardItem
-          donation={{
-            id: 12,
-            count: 200,
-            displayName: 'Toto',
-            email: 'toto@gmail.com',
-            mobile: 'mobile',
-            team: 'team',
-            message: 'message',
-            createdAt: "2022-02-24T14:24:08.311Z",
-          }}
-        />
-        <LeaderBoardItem
-          donation={{
-            id: 12,
-            count: 200,
-            displayName: 'Toto',
-            email: 'toto@gmail.com',
-            mobile: 'mobile',
-            team: 'team',
-            message: 'message',
-            createdAt: "2022-02-24T14:24:08.311Z",
-          }}
-        />
+        {data?.donations &&
+          data.donations.map((donation: Donation, idx: Key) => (
+            <LeaderBoardItem key={idx} donation={donation} />
+          ))}
       </VStack>
     </Box>
   );
