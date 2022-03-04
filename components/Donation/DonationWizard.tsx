c816e9a3e43d638260905b66842942f0ef98526e
Franck Fernandez <franck.fernandez@gmail.com>
2022-03-04 11:08:22 +0100

    Implement DonationDetails form and gql mutation

--- components/Donation/DonationWizard.tsx
+++ components/Donation/DonationWizard.tsx
@@ -1,55 +1,72 @@
-import { Box, Button } from '@chakra-ui/react';
-import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
-import { memo, useCallback, useEffect, useMemo, useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import { Box } from '@chakra-ui/layout';
+import React, { memo, useState } from 'react';
 import CountSelection from './CountSelection';
+import DonationDetails from './DonationDetails';
+
+const CREATE_DONATION_MUTATION = gql`
+  mutation CreateDonation($createDonationInput: CreateDonationInput!) {
+    createDonation(createDonationInput: $createDonationInput) {
+      count
+    }
+  }
+`;
 
 interface Props {}
 
-function DonationWizard({}: Props) {
-  const [step, setStep] = useState<number>(0);
+function DonationWizard(props: Props) {
+  const [step, setStep] = useState(0);
   const [donationDetails, setDonationDetails] = useState({
     count: 20,
   });
+  const [createDonation, { data }] = useMutation(CREATE_DONATION_MUTATION, {
+    refetchQueries: ['Donations'],
+  });
 
-  const next = useCallback((value: any = {}) => {
-    setDonationDetails((d) => ({ ...d, ...value }));
-    setStep((s) => (s >= pages.length - 1 ? pages.length - 1 : s + 1));
-  }, []);
+  const next = (values: any = {}) => {
+    const mergedDetails = { ...donationDetails, ...values };
 
-  const pages: ReactJSXElement[] = useMemo(
-    () => [
-      <CountSelection
-        key={0}
-        next={next}
-        initialCount={donationDetails.count}
-      />,
-      <div key={1}>Page 2</div>,
-    ],
-    [donationDetails.count, next]
-  );
+    if (step === pages.length - 1) {
+      submitDonation(mergedDetails);
+    } else {
+      setStep(step + 1);
+      setDonationDetails(mergedDetails);
+    }
+  };
 
-  const prev = useCallback(() => setStep((s) => (s <= 0 ? 0 : s - 1)), []);
+  const previous = () => {
+    setStep(step - 1);
+  };
+
+  const submitDonation = async (values: any) => {
+    await createDonation({
+      variables: {
+        createDonationInput: values,
+      },
+    });
+  };
+
+  const pages = [
+    <CountSelection key={0} next={next} initialCount={donationDetails.count} />,
+    <DonationDetails key={1} next={next} previous={previous} />,
+  ];
 
   return (
     <Box
       boxShadow="xl"
       p={8}
-      borderRadius="xl"
-      minW="sm"
       bgColor="blue.700"
       color="gray.200"
+      borderRadius="xl"
+      minW="sm"
     >
-      {pages[step]}
-      <Button
-        isFullWidth
-        colorScheme="blue"
-        variant="ghost"
-        size="lg"
-        borderRadius="full"
-        onClick={prev}
-      >
-        Previous
-      </Button>
+      {data ? (
+        <div>
+          Thank you for your donation of ${data?.createDonation?.count}!!
+        </div>
+      ) : (
+        pages[step]
+      )}
     </Box>
   );
 }
